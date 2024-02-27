6f0f9923af5c40ac6a7120895b5682efdfa117d3
Richtermnd <m.rustam1305@gmail.com>
2024-02-27 16:51:31 +0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Richtermnd/redirect
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	cutted, _ := strings.CutPrefix(r.URL.String(), "/tg")
+	http.Redirect(w, r, "tg:/"+cutted, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/tg/proxy", Redirect)
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		log.Fatal(err)
+	}
+}
