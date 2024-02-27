8ad1113d111edbe8e1eb91e4bc16153c98cca2e2
Richtermnd <m.rustam1305@gmail.com>
2024-02-27 17:14:23 +0500

    Add logging.

--- main.go
+++ main.go
@@ -8,11 +8,16 @@ import (
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
 	cutted, _ := strings.CutPrefix(r.URL.String(), "/tg")
-	http.Redirect(w, r, "tg:/"+cutted, http.StatusFound)
+	new := "tg:/" + cutted
+	log.Printf("Redirect from %s to %s", r.URL.String(), new)
+	http.Redirect(w, r, new, http.StatusFound)
 }
 
 func main() {
 	http.HandleFunc("/tg/proxy", Redirect)
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
 	log.Println("Start server")
 	if err := http.ListenAndServe(":9999", nil); err != nil {
 		log.Fatal(err)
