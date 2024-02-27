859b7279d276fda84e03455add3b8e775b4c2515
Richtermnd <m.rustam1305@gmail.com>
2024-02-27 20:26:08 +0500

    Little changes + Taskfile

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ WORKDIR "/app"
 COPY ["./go.mod", "./"]
 ADD . .
 
-EXPOSE 9999
+# Must be identical
+ENV PORT=8000
+EXPOSE 8000
 
 RUN CGO_ENABLED=0 GOOS=linux go build -o ./bin ./
 
--- main.go
+++ main.go
@@ -1,25 +1,46 @@
 package main
 
+// Server redirect from http://example.com/tg/proxy to tg://proxy
+
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 )
 
+var (
+	Port string
+)
+
 func Redirect(w http.ResponseWriter, r *http.Request) {
-	cutted, _ := strings.CutPrefix(r.URL.String(), "/tg")
-	new := "tg:/" + cutted
+	cutted, _ := strings.CutPrefix(r.URL.String(), "/tg/")
+	new := "tg://" + cutted
 	log.Printf("Redirect from %s to %s", r.URL.String(), new)
 	http.Redirect(w, r, new, http.StatusFound)
 }
 
 func main() {
+	// Load port from enviroment
+	loadEnv()
+
+	// Set handler
 	http.HandleFunc("/tg/proxy", Redirect)
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
-	})
+
+	// Start server
 	log.Println("Start server")
-	if err := http.ListenAndServe(":9999", nil); err != nil {
+	if err := http.ListenAndServe(":"+Port, nil); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func loadEnv() {
+	Port = os.Getenv("PORT")
+	if Port == "" {
+		log.Fatal("PORT env var not found")
+	}
+	if _, err := strconv.ParseInt(Port, 10, 64); err != nil {
+		log.Fatalf("PORT %s is not integer", Port)
 	}
 }
